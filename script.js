d0c76f48bad6fa69a14a42f2d24decf5ee47c857
Anurag Singh <anuragaswal27@gmail.com>
2024-06-27 23:35:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var hour = document.querySelector(".hours");
+var minute = document.querySelector(".minutes");
+var second = document.querySelector(".second");
+
+
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    hrotation = 30*htime + mtime/2;
+    mrotaion = 6*mtime;
+    srotation = 6*stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotaion}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+},1000);
